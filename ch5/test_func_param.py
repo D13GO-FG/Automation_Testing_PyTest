f489ba4acb23152c688b394228b6a4be45fccdce
Diego_Flores <ldiego.flores98@gmail.com>
2023-06-02 23:47:28 -0700

    Parametrizing Functions

---
+++ ch5/test_func_param.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+from tempfile import TemporaryDirectory
+import pytest
+from cards import Card
+import cards
+
+
+@pytest.fixture()
+def cards_db():
+    with TemporaryDirectory() as db_dir:
+        db_path = Path(db_dir)
+        db = cards.CardsDB(db_path)
+        yield db
+        db.close()
+
+
+@pytest.mark.parametrize(
+    "start_summary, start_state",
+    [
+        ("write a book", "done"),
+        ("second edition", "in prog"),
+        ("create a course", "todo")
+    ],
+)
+def test_finish(cards_db, start_summary, start_state):
+    initial_card = Card(summary=start_summary, state=start_state)
+    index = cards_db.add_card(initial_card)
+
+    cards_db.finish(index)
+
+    card = cards_db.get_card(index)
+    assert card.state == "done"
+
+
+@pytest.mark.parametrize("start_state", ["done", "in prog", "todo"])
+def test_finish_simple(cards_db, start_state):
+    c = Card("write a book", state=start_state)
+    index = cards_db.add_card(c)
+    cards_db.finish(index)
+    card = cards_db.get_card(index)
+    assert card.state == "done"
