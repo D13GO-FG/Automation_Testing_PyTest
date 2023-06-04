f7807704eb4a8bb39b8f97f09473e18cd5408927
Diego_Flores <ldiego.flores98@gmail.com>
2023-06-03 19:51:33 -0700

    Parametrizing with pytest_generate_tests

---
+++ ch5/test_gen.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from cards import Card
+import pytest
+import cards
+
+# pytest -v test_gen.py
+
+
+@pytest.fixture()
+def cards_db():
+    with TemporaryDirectory() as db_dir:
+        db_path = Path(db_dir)
+        db = cards.CardsDB(db_path)
+        yield db
+        db.close()
+
+
+def pytest_generate_tests(metafunc):
+    if "start_state" in metafunc.fixturenames:
+        metafunc.parametrize("start_state", ["done", "in prog", "todo"])
+
+
+def test_finish(cards_db, start_state):
+    c = Card("write a book", state=start_state)
+    index = cards_db.add_card(c)
+    cards_db.finish(index)
+    card = cards_db.get_card(index)
+    assert card.state == "done"
