20241614ebf928085df10149df13d26cfd89ad21
Diego_Flores <ldiego.flores98@gmail.com>
2023-06-04 18:00:22 -0700

    Skipping Tests Conditionally with pytest.mark.skipif

---
+++ ch6/builtins/test_skipif.py
@@ -0,0 +1,14 @@
+import pytest
+import cards
+from packaging.version import parse
+from cards import Card
+
+
+@pytest.mark.skipif(
+    parse(cards.__version__).major < 2,
+    reason="Card < comparison not supported in 1.x"
+)
+def test_less_than():
+    c1 = Card("a task")
+    c2 = Card("b task")
+    assert c1 < c2
