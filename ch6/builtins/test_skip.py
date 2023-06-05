71ceedfa2245736ec44b847f84ab3e430c287c97
Diego_Flores <ldiego.flores98@gmail.com>
2023-06-04 17:46:10 -0700

    Skipping Tests with pytest.mark.skip

---
+++ ch6/builtins/test_skip.py
@@ -0,0 +1,18 @@
+from cards import Card
+import pytest
+
+# pytest test_skip.py
+# pytest -v -ra test_skip.py --> Must display reason
+
+
+@pytest.mark.skip(reason="Card doesn't support < comparison yet")
+def test_less_than():
+    c1 = Card("a task")
+    c2 = Card("b task")
+    assert c1 < c2
+
+
+def test_equality():
+    c1 = Card("a task")
+    c2 = Card("a task")
+    assert c1 == c2
